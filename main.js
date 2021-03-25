7bc8d5da9db87329fbbcc9426c4390c957708492
UrosCeh <urosvceh@gmail.com>
2021-03-25 19:58:45 +0100

    Backspace function added

--- main.js
+++ main.js
@@ -1,16 +1,13 @@
 const number_buttons = document.getElementsByName("number")
-const equation = document.getElementById("equation")
+const equation = document.getElementById("equation-first")
 
-function display_function() {
-    for(let i = 0; i<number_buttons.length; i++) {
-        let btn = number_buttons[i]
-        btn.addEventListener('click', () => {
-            print_to_equation(btn.innerText, equation)
-        })
-    }
 
+for(let i = 0; i<number_buttons.length; i++) {
+    let btn = number_buttons[i]
+    btn.addEventListener('click', () => {
+        print_to_equation(btn.innerText, equation)
+    })
 }
-display_function()
 
 function print_to_equation(num, dest) {
     let val = dest.value
@@ -27,3 +24,25 @@ function print_to_equation(num, dest) {
     }
     dest.value = val
 }
+
+function backspace() {
+    equation.value = equation.value.slice(0, -1)
+}
+
+function sum() {
+    
+}
+
+// function change_output() {
+//     make_second_equation()
+//     const equation = document.getElementById("equation-second")
+// }
+
+// function make_second_equation() {
+//     const input = document.createElement("input")
+//     input.type = "button"
+//     input.id = "equation-second"
+//     input.classList.add("equation")
+//     input.value = "0"
+//     document.getElementById("arguments").appendChild(input)
+// }
