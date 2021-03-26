f6fad37304df65b5ae968791ccad5d778a8b898a
UrosCeh <urosvceh@gmail.com>
2021-03-26 18:23:47 +0100

    Enabled all functions, logic update needed

--- newmain.js
+++ newmain.js
@@ -3,8 +3,9 @@ const operator_buttons = document.getElementsByName("operator")
 const display = document.getElementById("display")
 const display_result = document.getElementById("display-result")
 const calculator = document.getElementById("calculator")
-var result = 0
+// var result = 0
 var values = new Array()
+
 
 for (let i = 0; i < number_buttons.length; i++) {
     number_buttons[i].addEventListener("click", () => {
@@ -14,7 +15,12 @@ for (let i = 0; i < number_buttons.length; i++) {
 
 for (let i = 0; i < operator_buttons.length; i++) {
     operator_buttons[i].addEventListener("click", () => {
-        // operator_buttons[i].style.backgroundColor = "red"
+        operator_buttons[i].style.border = "1px solid black"
+        for (let j = 0; j < operator_buttons.length; j++) {
+            if (i != j) {
+                operator_buttons[j].style.border = "none"
+            }
+        }
         const action = operator_buttons[i].dataset.action
         operation_in_progress(action)
     })
@@ -33,15 +39,84 @@ function display_number(value) {
 }
 
 function operation_in_progress(action) {
-    values.push(display.value)
     if (action == "sum") {
         sum()
+    }
+    if (action == "subtract") {
+        sub()
+    }
+    if (action == "multiply") {
+        mul()
+    }
+    if (action == "divide") {
+        div()
+    }
+    if (action == "equals") {
+        eq()
     }
     display.value = "0"
 }
 
 function sum() {
-    console.log(values)
-    result = parseFloat(result) + parseFloat(values[values.length-1])
+    let val = parseFloat(display.value)
+    if (values.length == 0) {
+        values.push(val)
+    }
+    else {
+        let res = values[values.length-1] + val
+        values.push(res)
+        console.log(values)
+    }
+    display_result.value = values[values.length-1]
+}
+
+function sub() {
+    let val = parseFloat(display.value)
+    if (values.length == 0) {
+        values.push(val)
+    }
+    else {
+        let res = values[values.length-1] - val
+        values.push(res)
+        console.log(values)
+    }
+    display_result.value = values[values.length-1]
+}
+
+function mul() {
+    let val = parseFloat(display.value)
+    if (values.length == 0) {
+        values.push(val)
+    }
+    else {
+        let res = values[values.length-1] * val
+        values.push(res)
+        console.log(values)
+    }
+    display_result.value = values[values.length-1]
+}
+
+function div() {
+    let val = parseFloat(display.value)
+    if (values.length == 0) {
+        values.push(val)
+    }
+    else {
+        let res = values[values.length-1] / val
+        values.push(res)
+        console.log(values)
+    }
+    display_result.value = values[values.length-1]
+}
+
+function eq() {
+    result = parseFloat(result) - parseFloat(values[values.length-1])
     display_result.value = result
+}
+
+function backspace() {
+    display.value = display.value.slice(0, -1)
+    if (display.value == "") {
+        display.value = "0"
+    }
 }
